fe7a95882c63227a985e0a5bdf56f0c936d15f6d
Ittipol Botmoon <ittipolbotmoon@Ittipols-MacBook-Air.local>
2026-03-20 23:27:53 +0700

    feat: implement order details API endpoint

--- spring-boot-rest/src/main/java/com/ahasan/rest/repo/OrderItemAddonRepo.java
+++ spring-boot-rest/src/main/java/com/ahasan/rest/repo/OrderItemAddonRepo.java
@@ -1,8 +1,9 @@
 package com.ahasan.rest.repo;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ahasan.rest.entity.OrderItemAddonEntity;
 
 public interface OrderItemAddonRepo extends JpaRepository<OrderItemAddonEntity, Integer> {
+	List<OrderItemAddonEntity> findByOrderItemId(Integer orderItemId);
 }
--- spring-boot-rest/src/main/java/com/ahasan/rest/repo/OrderItemRepo.java
+++ spring-boot-rest/src/main/java/com/ahasan/rest/repo/OrderItemRepo.java
@@ -1,8 +1,9 @@
 package com.ahasan.rest.repo;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ahasan.rest.entity.OrderItemEntity;
 
 public interface OrderItemRepo extends JpaRepository<OrderItemEntity, Integer> {
+	List<OrderItemEntity> findByOrderId(Integer orderId);
 }
