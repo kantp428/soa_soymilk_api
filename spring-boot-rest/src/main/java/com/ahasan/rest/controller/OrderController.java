54b8c49473f2eb5d9c4bfbb7052f24bfe0ebbe34
mrktp <misterkantapat@gmail.com>
2026-03-31 00:21:04 +0700

    feat(order): include order_id in the order map response

--- spring-boot-rest/src/main/java/com/ahasan/rest/controller/OrderController.java
+++ spring-boot-rest/src/main/java/com/ahasan/rest/controller/OrderController.java
@@ -118,6 +118,7 @@ public class OrderController extends ApiControllerSupport {
 
 	private Map<String, Object> toOrderMap(OrderEntity entity) {
 		Map<String, Object> data = new LinkedHashMap<String, Object>();
+		data.put("order_id", entity.getOrderId());
 		data.put("customer_id", entity.getCustomerId());
 		data.put("order_time", entity.getCreatedAt());
 		data.put("total_price", entity.getTotalPrice());
